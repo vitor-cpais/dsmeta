96f836de6437de8bacd3af5e4864c1a533d95396
Vitor C. Pais <vitor.cpais@gmail.com>
2022-09-07 15:10:43 -0300

    date select

---
+++ backend/src/main/java/com/nero/dsmeta/controller/SaleController.java
@@ -0,0 +1,34 @@
+package com.nero.dsmeta.controller;
+
+
+import com.nero.dsmeta.entities.Sale;
+
+import com.nero.dsmeta.service.SaleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleController {
+
+    @Autowired
+    private SaleService service;
+
+    @GetMapping
+    public Page<Sale> findSales(
+            @RequestParam(value="minDate", defaultValue = "")
+            String minDate,
+            @RequestParam(value="maxDate", defaultValue = "")
+            String maxDate,
+            Pageable pageable){
+
+        return service.findSales(minDate, maxDate, pageable);
+    }
+
+}
