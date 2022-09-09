06aff7b921833deadbbc6251d7ce423c69627c97
Vitor C. Pais <vitor.cpais@gmail.com>
2022-09-09 13:52:14 -0300

    send notification

--- frontend/src/componets/NotificationButton/index.tsx
+++ frontend/src/componets/NotificationButton/index.tsx
@@ -1,10 +1,28 @@
-import icon from '../../assets/img/notification-icon.svg'
-import './styles.css'
-function NotificationButton() {
+import icon from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from '../../utils/request';
+import './styles.css';
+import axios from 'axios';
+
+
+type Props = {
+
+    saleId: number;
+
+}
+
+function handleClick(saleId: number){
+    axios(`${BASE_URL}/sales/${saleId}/notification`)
+    .then(Response => {
+        console.log("certo")
+    })
+
+}
+
+function NotificationButton({saleId} : Props) {
     return (
 
         
-            <div className="dsmeta-red-btn">
+            <div className="dsmeta-red-btn" onClick={() => handleClick(saleId) }>
                 <img src={icon} alt="Notificar" />
             </div>
        
@@ -13,4 +31,4 @@ function NotificationButton() {
 
 }
 
-export default NotificationButton
+export default NotificationButton;
